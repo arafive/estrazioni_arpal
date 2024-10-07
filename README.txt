0376c5560b24f306c18f52ef7f868b0f09c8325b
Daniele Carnevale <lacaselladidaniele@gmail.com>
2024-10-07 16:36:31 +0200

    Aggiunta lettura e controllo su file coordinate
    Note
    Aggiunta descrizione della formattazione del file delle coordinate su README.txt

--- README.txt
+++ README.txt
@@ -1,1 +1,3 @@
 
+In config.ini, "percorso_file_coordinate" deve essere il path (assoluto) + file del .csv delle coordinate.
+Il file deve avere l'indice univoco per ogni stazione, una colonna 'Latitude' e una 'Longitude'.
--- estrazione_ecita.py
+++ estrazione_ecita.py
@@ -8,4 +8,8 @@ import configparser
 config = configparser.ConfigParser()
 config.read('./config.ini')
 
+df_file_coordinate = pd.read_csv(config.get('COMMON', 'percorso_file_coordinate'), index_col=0)
+assert 'Latitude' in df_file_coordinate.columns
+assert 'Longitude' in df_file_coordinate.columns
+
 print('\n\nDone')
