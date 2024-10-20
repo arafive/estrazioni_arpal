1edef55383289001623a6ccd076fd50fa96bcd4a
Daniele Carnevale <lacaselladidaniele@gmail.com>
2024-10-20 10:17:29 +0200

    Aggiunta cond esistenta per file tmp/ in concat

--- concatenazioni.py
+++ concatenazioni.py
@@ -52,108 +52,118 @@ lista_variabili = sorted([x for x in os.listdir(f'{cartella_madre_estrazione}/{o
 
 def f_concatenazione(v):
 # for v in lista_variabili:
-    df_v = pd.DataFrame()
-
-    # lista_cartelle_an_fc = sorted(os.listdir(f'{cartella_madre_estrazione}/{ora_start_forecast}/{v}'))
-    # for f in lista_cartelle_an_fc:
-    f = 'fc'  # Non prendo l'analisi
-
-    lista_livelli = sorted(os.listdir(f'{cartella_madre_estrazione}/{ora_start_forecast}/{v}/{f}'))
-
-    for l in lista_livelli:
-        if l == 'potentialVorticity':
-            v_nome = f'{v}2PVU'
-        else:
-            v_nome = v
-
-        global lista_stazioni
-        lista_stazioni = sorted(os.listdir(f'{cartella_madre_estrazione}/{ora_start_forecast}/{v}/{f}/{l}'))
-
-        for s in lista_stazioni:
-            t_inizio_s = time.time()
-            f_log_ciclo_for([['Variabile ', v, lista_variabili],
-                             ['Stazione ', s, lista_stazioni]])
-
-            df_s = pd.DataFrame()
-
-            lista_file_tempi = sorted(os.listdir(f'{cartella_madre_estrazione}/{ora_start_forecast}/{v}/{f}/{l}/{s}'))
-            lista_datetime = pd.to_datetime([x.split('.')[0] for x in lista_file_tempi])
-
-            for t, d in zip(lista_file_tempi, lista_datetime):
-                # print(v, f, l, s, t)
-                cartella_df = f'{cartella_madre_estrazione}/{ora_start_forecast}/{v}/{f}/{l}/{s}'
-
-                try:
-                    df = pd.read_csv(f'{cartella_df}/{t}', index_col=0, parse_dates=True)
-                except pd.errors.EmptyDataError:
-                    ### ECMWF/00/cape/fc/surface/TESTI/2021-02-03.csv erano vuoti, non so perchè
-                    continue
-                
-                if '.' in df.columns[0]:
-                    ### Colpa mia, solo nell'ecita
-                    df.columns = [x.split('.')[0] for x in df.columns]
-                    df.columns = [f"{x.split('_')[1]}_{x.split('_')[0]}" for x in df.columns]
-                    
-                if len(df.columns[0].split('_')) == 2 and dict_config_modelli[config.get('CONCATENAZIONI', 'modello')] == 'ECMWF':
-                    ### Ecita non ha sempre avuto un numero costante di livelli in pressione.
-                    ### Devo tenere solo quelli sempre presenti.
-                    ### I venti tipo u10 non vengono tolti.
-
-                    livelli_hPa_da_togliere = [10] + list(np.arange(25, 325, 25))
-                    for livello_da_togliere in livelli_hPa_da_togliere:
-                        if livelli_hPa_da_togliere == 100:
-                            ### Altrimenti mi toglie anche 1000
-                            df = df.drop(columns=[f'100_{x}' for x in list(string.ascii_uppercase)[:punti_piu_vicini_da_estrarre]])
-                        else:
-                            df = df.drop(columns=[x for x in df.columns if f'{livello_da_togliere}_' in x])
-                
-                if dict_config_modelli[config.get('CONCATENAZIONI', 'modello')] == 'ECMWF' and v in ['tp', 'cp']:
-                    ### Ecita: non ha la tp3 ma una precipitazione cumualta dallo start fino alla fine
-                    df_shift = df.shift(periods=1, fill_value=0) # periods=1 per la tp3, nota bene che oltre +96 diventa ogni 6 ore
-                    df = df - df_shift
-
-                if df.index[0].hour == 0:
-                    ### Ecita: dal 2023-01-14 l'analisi e il forecast non sono più separati. Devo togliere la prima riga
-                    df = df.drop(df.index[0], axis=0)
-
-                freq = '1h' if dict_config_modelli[config.get('CONCATENAZIONI', 'modello')] == 'MOLOCHsfc' else '3h'
-                df = df.loc[df.index.intersection(pd.date_range(d + pd.DateOffset(hours=int(range_previsionale.split('-')[0])),
-                                                                d + pd.DateOffset(hours=int(range_previsionale.split('-')[1])),
-                                                                freq=freq))]
-
-                v_nome = v_nome.replace('_', '')  # Nel caso di 'cape_con' -> 'capecon'
-                df.columns = [f'{v_nome}_{x}_{s}' for x in df.columns]
-
-                df_s = pd.concat([df_s, df], axis=0)
-
-            try:
-                df_v = pd.concat([df_v, df_s], axis=1)
-            except pd.errors.InvalidIndexError:
-                ### Problemi con ECMWF/00/cape/fc/surface/TESTI/2023-12-31.csv
-                continue
-
-    lista_completa_datetime = pd.date_range(lista_datetime[0], lista_datetime[-1] + pd.DateOffset(hours=24), freq=freq)
-
-    ### Per non rischiare di generare un mostro di .csv, devo salvare a pezzetti.
-
-    if range_previsionale == '24-48' or range_previsionale == '48-72':
-        ### Tengo la prima previsione successiva
-        df_v = df_v[~df_v.index.duplicated(keep='last')]
+    
+    nome_df_finale = f"{cartella_tmp}/df_{v}_{range_previsionale}_{dict_config_modelli[config.get('CONCATENAZIONI', 'modello')]}_{config.get('CONCATENAZIONI', 'regione')}.csv"
+    
+    if os.path.exists(nome_df_finale):
+        print(f'{nome_df_finale} esiste già. Continuo.')
+        # continue
+        return
+    
+    else:
         
-    df_v = df_v.reindex(lista_completa_datetime, fill_value=np.nan)
-    df_v = df_v.sort_index()
-    df_v = df_v.dropna()
-
-    df_v.to_csv(f"{cartella_tmp}/df_{v}_{range_previsionale}_{dict_config_modelli[config.get('CONCATENAZIONI', 'modello')]}_{config.get('CONCATENAZIONI', 'regione')}.csv", index=True, header=True, mode='w', na_rep=np.nan)
-
-    f_printa_tempo_trascorso(t_inizio_s, time.time(), nota=f'Tempo per variabile {v}')
-    print()
-
+        df_v = pd.DataFrame()
+    
+        # lista_cartelle_an_fc = sorted(os.listdir(f'{cartella_madre_estrazione}/{ora_start_forecast}/{v}'))
+        # for f in lista_cartelle_an_fc:
+        f = 'fc'  # Non prendo l'analisi
+    
+        lista_livelli = sorted(os.listdir(f'{cartella_madre_estrazione}/{ora_start_forecast}/{v}/{f}'))
+    
+        for l in lista_livelli:
+            if l == 'potentialVorticity':
+                v_nome = f'{v}2PVU'
+            else:
+                v_nome = v
+    
+            global lista_stazioni
+            lista_stazioni = sorted(os.listdir(f'{cartella_madre_estrazione}/{ora_start_forecast}/{v}/{f}/{l}'))
+    
+            for s in lista_stazioni:
+                t_inizio_s = time.time()
+                f_log_ciclo_for([['Variabile ', v, lista_variabili],
+                                 ['Stazione ', s, lista_stazioni]])
+    
+                df_s = pd.DataFrame()
+    
+                lista_file_tempi = sorted(os.listdir(f'{cartella_madre_estrazione}/{ora_start_forecast}/{v}/{f}/{l}/{s}'))
+                lista_datetime = pd.to_datetime([x.split('.')[0] for x in lista_file_tempi])
+    
+                for t, d in zip(lista_file_tempi, lista_datetime):
+                    # print(v, f, l, s, t)
+                    cartella_df = f'{cartella_madre_estrazione}/{ora_start_forecast}/{v}/{f}/{l}/{s}'
+    
+                    try:
+                        df = pd.read_csv(f'{cartella_df}/{t}', index_col=0, parse_dates=True)
+                    except pd.errors.EmptyDataError:
+                        ### ECMWF/00/cape/fc/surface/TESTI/2021-02-03.csv erano vuoti, non so perchè
+                        continue
+                    
+                    if '.' in df.columns[0]:
+                        ### Colpa mia, solo nell'ecita
+                        df.columns = [x.split('.')[0] for x in df.columns]
+                        df.columns = [f"{x.split('_')[1]}_{x.split('_')[0]}" for x in df.columns]
+                        
+                    if len(df.columns[0].split('_')) == 2 and dict_config_modelli[config.get('CONCATENAZIONI', 'modello')] == 'ECMWF':
+                        ### Ecita non ha sempre avuto un numero costante di livelli in pressione.
+                        ### Devo tenere solo quelli sempre presenti.
+                        ### I venti tipo u10 non vengono tolti.
+    
+                        livelli_hPa_da_togliere = [10] + list(np.arange(25, 325, 25))
+                        for livello_da_togliere in livelli_hPa_da_togliere:
+                            if livelli_hPa_da_togliere == 100:
+                                ### Altrimenti mi toglie anche 1000
+                                df = df.drop(columns=[f'100_{x}' for x in list(string.ascii_uppercase)[:punti_piu_vicini_da_estrarre]])
+                            else:
+                                df = df.drop(columns=[x for x in df.columns if f'{livello_da_togliere}_' in x])
+                    
+                    if dict_config_modelli[config.get('CONCATENAZIONI', 'modello')] == 'ECMWF' and v in ['tp', 'cp']:
+                        ### Ecita: non ha la tp3 ma una precipitazione cumualta dallo start fino alla fine
+                        df_shift = df.shift(periods=1, fill_value=0) # periods=1 per la tp3, nota bene che oltre +96 diventa ogni 6 ore
+                        df = df - df_shift
+    
+                    if df.index[0].hour == 0:
+                        ### Ecita: dal 2023-01-14 l'analisi e il forecast non sono più separati. Devo togliere la prima riga
+                        df = df.drop(df.index[0], axis=0)
+    
+                    freq = '1h' if dict_config_modelli[config.get('CONCATENAZIONI', 'modello')] == 'MOLOCHsfc' else '3h'
+                    df = df.loc[df.index.intersection(pd.date_range(d + pd.DateOffset(hours=int(range_previsionale.split('-')[0])),
+                                                                    d + pd.DateOffset(hours=int(range_previsionale.split('-')[1])),
+                                                                    freq=freq))]
+    
+                    v_nome = v_nome.replace('_', '')  # Nel caso di 'cape_con' -> 'capecon'
+                    df.columns = [f'{v_nome}_{x}_{s}' for x in df.columns]
+    
+                    df_s = pd.concat([df_s, df], axis=0)
+    
+                try:
+                    df_v = pd.concat([df_v, df_s], axis=1)
+                except pd.errors.InvalidIndexError:
+                    ### Problemi con ECMWF/00/cape/fc/surface/TESTI/2023-12-31.csv
+                    continue
+    
+        lista_completa_datetime = pd.date_range(lista_datetime[0], lista_datetime[-1] + pd.DateOffset(hours=24), freq=freq)
+    
+        ### Per non rischiare di generare un mostro di .csv, devo salvare a pezzetti.
+    
+        if range_previsionale == '24-48' or range_previsionale == '48-72':
+            ### Tengo la prima previsione successiva
+            df_v = df_v[~df_v.index.duplicated(keep='last')]
+            
+        df_v = df_v.reindex(lista_completa_datetime, fill_value=np.nan)
+        df_v = df_v.sort_index()
+        df_v = df_v.dropna()
+    
+        df_v.to_csv(nome_df_finale, index=True, header=True, mode='w', na_rep=np.nan)
+    
+        f_printa_tempo_trascorso(t_inizio_s, time.time(), nota=f'Tempo per variabile {v}')
+        print()
+    
 # # # # # # # #   # # # # # # # #   # # # # # # # #
 # # # # # # # #   # # # # # # # #   # # # # # # # #
 # # # # # # # #   # # # # # # # #   # # # # # # # #
-
-
+    
+    
 if int(config.get('CONCATENAZIONI', 'job')) == 0:
     ### Ciclo sulle variabili
     for v in lista_variabili:
@@ -172,11 +182,14 @@ else:
 
 print('\nCreazione dei dataset delle singole stazioni\n')
 
+lista_stazioni = pd.read_csv(config.get('COMMON', 'percorso_file_coordinate'), index_col=0).index.tolist()
+
 for s in lista_stazioni:
     f_log_ciclo_for([['Stazione ', s, lista_stazioni]])
 
     df_s = pd.DataFrame()
 
+    print('Apro i dataset...')
     for v in lista_variabili:
         df_v = pd.read_csv(f"{cartella_tmp}/df_{v}_{range_previsionale}_{dict_config_modelli[config.get('CONCATENAZIONI', 'modello')]}_{config.get('CONCATENAZIONI', 'regione')}.csv", index_col=0, parse_dates=True)
         df_s = pd.concat([df_s, df_v[[x for x in df_v if s in x]]], axis=1)
